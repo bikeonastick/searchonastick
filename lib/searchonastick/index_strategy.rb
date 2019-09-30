0080cc08cf98be81f4715a4a1dd28c3649a3f9c4
bikeonastick <bikeonastick@gmail.com>
2019-09-29 23:41:18 -0500

    adding reuse of index and first benchmark measurements

--- lib/searchonastick/index_strategy.rb
+++ lib/searchonastick/index_strategy.rb
@@ -16,6 +16,7 @@ module Searchonastick
       @results = []
       @keep_index = keep
       @reuse_index = reuse
+      @ix_file_name = 'index_files/index_word_storage'
     end
 
     def search(text_files, search_string)
@@ -34,18 +35,49 @@ module Searchonastick
       return @results
     end
 
+    # 
+    # Build an index for searching files.
+    #
     def index(text_files)
-      @index_words = Searchonastick::IndexWords.new(text_files.docs)
-      text_files.docs.each{|file_name|
-        IO.foreach(file_name){|line|
-          line_words = line.split(" ")
-          line_words.each{|word|
-            #TODO: should i remove punctuation? Probably want to keep
-            #apostrophes
-            @index_words.add_word(word,file_name)
+      if(@reuse_index && File.exist?(@ix_file_name))
+        @index_words = load_index
+      else
+        @index_words = Searchonastick::IndexWords.new(text_files.docs)
+        text_files.docs.each{|file_name|
+          IO.foreach(file_name){|line|
+            line_words = line.split(" ")
+            line_words.each{|word|
+              #TODO: should i remove punctuation? Probably want to keep
+              #apostrophes
+              @index_words.add_word(word,file_name)
+            }
           }
         }
-      }
+      end
+      if(@keep_index)
+        dump_index
+      end
     end
+
+    #
+    # Loads a dumped version of the IndexWords instance
+    #
+    def load_index
+      ix_file = File.open(@ix_file_name,'r')
+      ix_data = ix_file.read()
+      ix_file.close()
+      Marshal.load(ix_data)
+    end
+
+    #
+    # Dumps current state of IndexWords to a file
+    #
+    def dump_index
+      ix_file=File.open(@ix_file_name,'w')
+      the_index = Marshal.dump(@index_words)
+      ix_file.puts(the_index)
+      ix_file.close()
+    end
+
   end
 end
--- lib/searchonastick/searcher.rb
+++ lib/searchonastick/searcher.rb
@@ -1,6 +1,7 @@
 require 'searchonastick/regex_strategy'
 require 'searchonastick/scan_strategy'
 require 'searchonastick/index_strategy'
+require 'benchmark'
 
 module Searchonastick
   
@@ -10,16 +11,22 @@ module Searchonastick
     REGEX = 1
     INDEXED = 2
 
-    attr_accessor :strategy, :search_string, :text_files, :results, :search_impl
+    attr_accessor :strategy, :search_string, :text_files, :results, 
+      :search_impl, :keep_index, :reuse_index 
+
+    attr_reader :index_benchmark, :search_benchmark
 
     @search_string
     @text_files
     @results
 
-    def initialize(strategy=SIMPLE,text_files=[])
+    def initialize(strategy=SIMPLE, text_files=[], search_string="", ki=false, ri=false)
       @strategy = strategy
       @text_files = text_files
+      @keep_index = ki
+      @reuse_index = ri
       @search_impl = search_factory
+      @search_string = search_string
     end
 
     def search_factory
@@ -29,8 +36,8 @@ module Searchonastick
       when REGEX
         @search_impl = RegexStrategy.new
       when INDEXED
-        @search_impl = IndexStrategy.new
-        @search_impl.index(@text_files)
+        @search_impl = IndexStrategy.new(@keep_index, @reuse_index)
+        @index_benchmark = Benchmark.measure{@search_impl.index(@text_files)}
         @search_impl
       else
         "error"
@@ -38,7 +45,10 @@ module Searchonastick
     end
 
     def search
-      @results = @search_impl.search(@text_files,@search_string)
+      @search_benchmark = Benchmark.measure{
+        @results = @search_impl.search(@text_files,@search_string)
+      }
+      return @results
     end
   end
 end
